40fa833f14f6db5c5c575b9c2686e09d9c469d3f
Yuri-Kravets <kravets.yuri.v@gmail.com>
2022-01-30 11:15:03 +0200

    update

---
+++ js/script.js
@@ -0,0 +1,63 @@
+"use strict";
+(function() {
+    const form = document.querySelector('#form');
+    
+    form.addEventListener('submit', event => {
+        event.preventDefault();
+        
+        const inpData = Array.from(
+            event.target.querySelectorAll('input,select,textarea,checkbox')
+        )
+            .reduce((accum,item) => {
+                accum[item.name] = item.value;
+                return accum;
+            },{});
+            
+        localStorage.setItem (
+            'inputData',
+            JSON.stringify(inpData)
+        );
+        console.log(
+            JSON.parse(
+                localStorage.getItem('inputData')
+            )
+        );    
+        //makeDocument(JSON.parse(localStorage.getItem('inputData')));
+
+    });
+    
+})();
+
+
+
+
+
+
+        const newDoc = document.implementation.createHTMLDocument(title)
+        function makeDocument() {
+            let frame = document.getElementById("theFrame");
+        
+            let doc = document.implementation.createHTMLDocument("New Document");
+            let p = doc.createElement("p");
+            p.innerHTML = "This is a new paragraph.";
+
+            try {
+                doc.body.appendChild(p);
+                } catch(e) {
+                    console.log(e);
+                }
+                let destDocument = frame.contentDocument;
+                let srcNode = doc.documentElement;
+                let newNode = destDocument.importNode(srcNode, true);
+                destDocument.replaceChild(newNode, destDocument.documentElement);
+            }
+
+
+
+
+
+  // let select = document.querySelector('#select-1');
+
+        // select.onchange = function () {
+        //     console.log(select.value); 
+        // }
